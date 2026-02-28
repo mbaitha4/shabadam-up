6e008c84a2090e181c8c9f5fa8bee7e5bc76c931
mbaitha4 <mbaitha4@gmail.com>
2026-02-28 18:46:49 +0530

    Create page.tsx

---
+++ app/naukri/page.tsx
@@ -0,0 +1,11 @@
+export default function NaukriPage() {
+  return (
+    <div>
+      <h2 className="section-title">नौकरी-भर्ती</h2>
+      <p>
+        यहाँ उत्तर प्रदेश की सरकारी एवं निजी नौकरियों
+        की जानकारी प्रकाशित की जाएगी।
+      </p>
+    </div>
+  );
+}
