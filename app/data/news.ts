980148846807e0a471e61dd7084bd4be84efe693
mbaitha4 <mbaitha4@gmail.com>
2026-02-28 18:40:11 +0530

    Create news.ts

---
+++ app/data/news.ts
@@ -0,0 +1,16 @@
+export const newsData = [
+  {
+    slug: "lucknow-vikas-yojana",
+    title: "लखनऊ में नई विकास योजना की घोषणा",
+    image: "https://images.unsplash.com/photo-1581090700227-1e8b0c1a4b26",
+    content:
+      "लखनऊ में नई विकास योजना की घोषणा की गई है। यह परियोजना शहर के बुनियादी ढांचे को मजबूत करेगी।",
+  },
+  {
+    slug: "cm-review-meeting",
+    title: "मुख्यमंत्री ने नई योजनाओं की समीक्षा की",
+    image: "https://images.unsplash.com/photo-1504711434969-e33886168f5c",
+    content:
+      "मुख्यमंत्री ने नई योजनाओं की समीक्षा की और अधिकारियों को आवश्यक निर्देश दिए।",
+  },
+];
