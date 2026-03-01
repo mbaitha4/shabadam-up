f208f51dc6016b6bde8ac35f6dec9996496f57d4
mbaitha4 <mbaitha4@gmail.com>
2026-03-01 06:14:26 +0530

    Update News.ts

--- models/News.ts
+++ models/News.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const NewsSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  image: String,
-  slug: { type: String, unique: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const NewsSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+    image: String,
+    slug: String,
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.News ||
   mongoose.model("News", NewsSchema);
