0d65e7ed0f1ac443189156d44b265f37c7988581
mbaitha4 <mbaitha4@gmail.com>
2026-03-05 17:45:55 +0530

    Update page.tsx

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,37 +1,37 @@
 import { connectDB } from "@/lib/mongodb";
 import News from "@/models/News";
 
-export default async function ArticlePage({ params }: any) {
+export default async function ArticlePage({ params }:any) {
 
   await connectDB();
 
-  const article = await News.findById(params.id).lean();
+  const article = await News.findById(params.id);
 
-  if (!article) return <div>Article not found</div>;
+  if (!article) {
+    return <div>Article not found</div>;
+  }
 
   return (
 
-    <div style={{ maxWidth: "900px", margin: "auto", padding: "20px" }}>
+    <div style={{maxWidth:"900px",margin:"auto",padding:"20px"}}>
 
       <h1>{article.title}</h1>
 
-      <p style={{ color: "gray" }}>
+      <p style={{color:"gray"}}>
         {new Date(article.createdAt).toLocaleString()}
       </p>
 
       {article.image && (
         <img
           src={article.image}
-          alt={article.title}
-          style={{ width: "100%", margin: "20px 0" }}
+          style={{width:"100%",margin:"20px 0"}}
         />
       )}
 
-      <div style={{ fontSize: "18px", lineHeight: "1.7" }}>
+      <p style={{fontSize:"18px",lineHeight:"1.7"}}>
         {article.content}
-      </div>
+      </p>
 
     </div>
-
   );
 }
